174fe14d7c5299e7fdf1f851cb725aea6334d2b9
hugosenari <hugosenari@gmail.com>
2022-03-12 02:03:53 -0300

    chore: re index webp

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''WebP Tools wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-webp-master.flake = false;
+  inputs.src-webp-master.owner = "juancarlospaco";
+  inputs.src-webp-master.ref   = "refs/heads/master";
+  inputs.src-webp-master.repo  = "nim-webp";
+  inputs.src-webp-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-webp-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-webp-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
