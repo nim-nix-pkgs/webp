222dc19de5686c79eef0aa2c0128ce0e2a4d3bfb
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:49 -0300

    chore: re index webp

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."webp-master".dir   = "master";
+  inputs."webp-master".owner = "nim-nix-pkgs";
+  inputs."webp-master".ref   = "master";
+  inputs."webp-master".repo  = "webp";
+  inputs."webp-master".type  = "github";
+  inputs."webp-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."webp-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
